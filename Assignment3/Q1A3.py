2f078d02abdbbd2b7325cae0847c40d188ae3de9
Jordzira <jdc.atkins@gmail.com>
2023-03-04 16:53:15 -0700

    q1a3 re-added indices

--- Assignment3/Q1A3.py
+++ Assignment3/Q1A3.py
@@ -127,7 +127,10 @@ def create_indexes():
     cursor.execute("DROP INDEX IF EXISTS orders_index;")
     cursor.execute("DROP INDEX IF EXISTS order_items_index;")
 
-    cursor.execute("CREATE INDEX customers_index ON Customers(customer_postal_code);")
+    cursor.execute("CREATE INDEX customers_index ON Customers(customer_id);")
+    cursor.execute("CREATE INDEX sellers_index ON Sellers(seller_id);")
+    cursor.execute("CREATE INDEX orders_index ON Orders(order_id);")
+    cursor.execute("CREATE INDEX order_items_index ON Order_items(order_id, order_item_id, product_id, seller_id);")
 
     
     conn.commit()
